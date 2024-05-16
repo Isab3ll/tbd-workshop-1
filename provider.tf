46619a5f7a4695033b124f4355c9589fd2b2ac7f
imarczew <01159043@pw.edu.pl>
2024-05-16 18:51:46 +0200

    Setup laboratorium 1

--- provider.tf
+++ provider.tf
@@ -26,9 +26,10 @@ provider "kubernetes" {
 }
 
 terraform {
-  required_version = "~> 1.8.3"
+  required_version = ">= 1.5.0"
   required_providers {
     google = {
+      source  = "hashicorp/google"
       version = "~> 5.23.0"
     }
     docker = {
